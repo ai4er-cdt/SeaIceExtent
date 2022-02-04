e112fb3f5c2d3c3305fa1fec4b4641ffd1370418
Sophie Turner <st838@cam.ac.uk>
2022-02-04 22:07:02 +0000

    Re-wrote tiling fns and added capability to tile triplets together.

--- functions.py
+++ functions.py
@@ -74,24 +74,41 @@ def upsample_all(in_path, out_path, new_resolution):
                 break
 
 
-def tile_all(in_path, out_path, tile_size_x, tile_size_y, step_x, step_y):
-    # Create tiles from all SAR images and generate their metadata.
+def tile_all(in_path, modis_path, out_path, tile_size, step_size):
+    # Create tiles from all SAR and MODIS images and generate their metadata.
     # Assumes images have already been through the RelabelAll function and placed in the raw training data directory.
     os.chdir(in_path)
     for item in os.listdir():
         # Avoid doubling the necessary number of string operations because they are in pairs.
         if item.endswith("labels.tif"):
-            # The first 18 chars are the same for each pair.
-            image_name = item[0:17]
+            # The first chars are the same for each pair.
+            date_name = item.split('_l')[0]
             labels_path = "{}\{}".format(in_path, item)
-            sar_path = "{}\{}_sar.tif".format(in_path, image_name)
-            # Get the geo info from the SAR tif.
-            sar_data = gdal.Open(sar_path)
-            geography = sar_data.GetGeoTransform()
+            sar_path = "{}\{}_sar.tif".format(in_path, date_name)
+            modis_path = "{}\{}_modis.tif".format(modis_path, date_name)
+            # Get the geo info from the SAR tif. Not all dates have a SAR image associated with them.
+            try:
+                image_data = gdal.Open(modis_path)
+                has_modis = True
+            except:
+                has_modis = False
+            try:
+                image_data = gdal.Open(sar_path)
+                has_sar = True
+            except:
+                has_sar = False
+            geography = image_data.GetGeoTransform()
             top_left = geography[0], geography[3]
-
-            tile_image(sar_path, labels_path, out_path, image_name, top_left, tile_size_x, tile_size_y, step_x, step_y,
-                       1, False)
+            image_data.FlushCache()
+            del image_data
+            if has_modis and has_sar:
+                tile_triplet(modis_path, sar_path, labels_path, date_name, top_left, tile_size, step_size)
+            elif has_modis:
+                tile_pair(modis_path, labels_path, out_path, date_name, "modis", top_left, tile_size, tile_size, step_size, step_size,
+                1, False)
+            elif has_sar:
+                tile_pair(sar_path, labels_path, out_path, date_name, "sar", top_left, tile_size, tile_size, step_size, step_size,
+                1, False)
 
 
 def shp2tif(shape_file, sar_raster, output_raster_name):
@@ -137,40 +154,81 @@ def shp2tif(shape_file, sar_raster, output_raster_name):
     output_raster_ds = None
 
 
-def tile_image(sar_tif, labelled_tif, output_directory, image_name, top_left, tile_size_x, tile_size_y, step_x, step_y,
+def tile_triplet(modis_path, sar_path, labels_path, out_path, date_name, top_left, tile_size, step_size):
+    modis_window = tif_to_window(modis_path, tile_size, step_size)
+    sar_window = tif_to_window(sar_path, tile_size, step_size)
+    labels_window = tif_to_window(labels_path, tile_size, step_size)
+    num_shape = sar_window.shape[1]
+    if modis_window.shape != sar_window.shape != labels_window.shape:
+        raise Exception(f'MODIS, SAR and label dimensions do not match with dimensions of '
+                        f'{modis_window.shape} and {sar_window.shape} and {labels_window.shape} respectively.')
+    out_path_full = "{}\{}".format(out_path, date_name)
+    for row_count, (row_modis, row_sar, row_labels) in enumerate(zip(modis_window, sar_window, labels_window)):
+        for tile_count, (tile_modis, tile_sar, tile_labels) in enumerate(zip(row_modis, row_sar, row_labels)):
+            tile_num = num_shape * row_count + tile_count
+            # Check if the label tile contains any unclassified / no data. Discard them if so.
+            if np.amin(tile_labels) == 0:
+                continue
+            n_water = np.count_nonzero(tile_labels == 100)
+            n_ice = np.count_nonzero(tile_labels == 200)
+            # Save the tiles.
+            modis_name = "{}_tile{}_modis.npy".format(out_path_full, tile_num)
+            sar_name = "{}_tile{}_sar.npy".format(out_path_full, tile_num)
+            labels_name = "{}_tile{}_labels.npy".format(out_path_full, tile_num)
+            np.save(modis_name, tile_modis)
+            np.save(sar_name, tile_sar)
+            np.save(labels_name, tile_labels)
+            # Update metadata.
+            generate_metadata(out_path, tile_num, date_name, n_water, n_ice, top_left, row_count, step_size, tile_count, step_size, tile_size)
+
+
+def tif_to_window(tif_path, tile_size, step_size):
+    image_tif = Image.open(tif_path)
+    image_array = np.asarray(image_tif)
+    image_tif.FlushCache()
+    del image_tif
+    image_window = np.lib.stride_tricks.sliding_window_view(x=image_array, window_shape=
+                                                           (tile_size, tile_size))[::step_size, ::step_size]
+    return image_window
+
+
+def tile_pair(image_tif, labelled_tif, output_directory, image_name, image_type, top_left, tile_size_x, tile_size_y, step_x, step_y,
                sea_ice_discard_proportion, verbose):
     """GTC Code to tile up a SAR-label tif pair according to the specified window sizes and save the tiles as
     .npy files. Any tile containing unclassified/no-data classes is rejected (not saved), as are tiles containing a
     disproportionate amount of a single class (water or ice). Set verbose to True to print the tiling metrics for each
     run."""
+    # image_type = "sar" or "modis"
 
     # Tifs
-    sar_tif = Image.open(sar_tif)
+    image_tif = Image.open(image_tif)
     label_tif = Image.open(labelled_tif)
 
     # Image Arrays
-    sar_tif_array = np.asarray(sar_tif)
+    image_tif_array = np.asarray(image_tif)
     label_tif_array = np.asarray(label_tif)
 
     # Defining sliding windows
-    window_view_sar = np.lib.stride_tricks.sliding_window_view(x=sar_tif_array, window_shape=
+    window_view_image = np.lib.stride_tricks.sliding_window_view(x=image_tif_array, window_shape=
                                                                (tile_size_x, tile_size_y))[::step_y, ::step_x]
     window_view_label = np.lib.stride_tricks.sliding_window_view(x=label_tif_array,window_shape=
                                                                  (tile_size_x, tile_size_y))[::step_y, ::step_x]
-    num = window_view_sar.shape[1]
+    num = window_view_image.shape[1]
 
     # Checking the sliding windows are the same dimensions
-    if window_view_sar.shape != window_view_label.shape:
-        raise Exception(f'SAR TIF and Labelled TIF dimensions do not match with dimensions of '
-                        f'{window_view_sar.shape} and {window_view_label.shape} respectively.')
+    if window_view_image.shape != window_view_label.shape:
+        raise Exception(f'Image TIF and Labelled TIF dimensions do not match with dimensions of '
+                        f'{window_view_image.shape} and {window_view_label.shape} respectively.')
 
     # Initialising counters
     n_unclassified = 0
     n_similar = 0
 
+    out_path_full = "{}\{}".format(output_directory, image_name)
+
     # Simultaneously iterating through each SAR and labelled tile and saving as a .npy file
-    for count1, (row_sar, row_label) in enumerate(zip(window_view_sar, window_view_label)):
-        for count2, (tile_sar, tile_label) in enumerate(zip(row_sar, row_label)):
+    for count1, (row_image, row_label) in enumerate(zip(window_view_image, window_view_label)):
+        for count2, (tile_image, tile_label) in enumerate(zip(row_image, row_label)):
 
             n = num * count1 + count2
 
@@ -190,8 +248,10 @@ def tile_image(sar_tif, labelled_tif, output_directory, image_name, top_left, ti
                 continue
 
             # There is scope here to use the numpy.savez_compressed function to improve efficiency.
-            np.save(output_directory + '\{}_sar.npy'.format(str(n)), tile_sar)
-            np.save(output_directory + '\{}_label.npy'.format(str(n)), tile_label)
+            image_name = "{}_tile{}_{}.npy".format(out_path_full, str(n), image_type)
+            labels_name = "{}_tile{}_labels.npy".format(out_path_full, str(n))
+            np.save(image_name, tile_image)
+            np.save(labels_name, tile_label)
 
             generate_metadata(output_directory, n, image_name, n_water, n_ice, top_left, count1, step_x, count2, step_y,
                               tile_size_x)
@@ -411,3 +471,6 @@ data = r"G:\Shared drives\2021-gtc-sea-ice\data"
 clipped = r"G:\Shared drives\2021-gtc-sea-ice\trainingdata\clipped"
 tiled = r"G:\Shared drives\2021-gtc-sea-ice\trainingdata\tiled"
 
+#opening the modis image
+#ERROR 4: G:\Shared drives\2021-gtc-sea-ice\trainingdata\clipped\2011-01-13_021245_modis.tif\2011-01-14_063311_modis.tif\2011-01-15_055843_modis.tif\2011-01-18_122137_modis.tif\2011-01-23_060458_modis.tif\2011-01-24_134238_modis.tif\2011-01-28_030409_modis.tif\2011-01-29_072859_modis.tif\2011-02-10_032730_modis.tif\2011-02-14_024135_modis.tif\2011-03-01_033109_modis.tif\2011-03-13_025215_modis.tif\2011-03-14_021541_modis.tif\2011-03-15_031817_modis.tif\2011-03-26_063245_modis.tif\2011-03-30_022935_modis.tif\2012-11-14_080136_modis.tif\2012-11-11_074900_modis.tif\2012-12-25_080637_modis.tif\2012-10-09_071140_modis.tif\2012-12-24_221605_modis.tif\2012-11-14_080243_modis.tif\2012-12-27_035034_modis.tif\2012-10-10_064110_modis.tif\2013-01-31_234751_modis.tif\2013-02-06_035454_modis.tif: No such file or directory
+#No modis image found
