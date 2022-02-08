bae581cef52623831302a1d62ad541070acf8101
Sophie Turner <st838@cam.ac.uk>
2022-02-08 10:56:11 +0000

    New file structure.

---
+++ SeaIce/our_code.py
@@ -0,0 +1,2 @@
+# Code which we run specifically on our dataset, which would not be applicable to others.
+# This code is not really reuseable unless given as an example.
---
+++ SeaIce/preprocessing/data_handling.py
@@ -0,0 +1,2 @@
+# Moves data around and writes output.
+# Might be where other functions are called from, and where their returns end up.
