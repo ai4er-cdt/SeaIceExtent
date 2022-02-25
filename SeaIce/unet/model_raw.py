880f38107849c413c9e291997f1cf9b86d3cd597
Sophie Turner <st838@cam.ac.uk>
2022-02-25 14:05:35 +0000

    Resolving merge conflicts.

--- SeaIce/unet/model_raw.py
+++ SeaIce/unet/model_raw.py
@@ -140,6 +140,7 @@ def get_args():
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
     parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
+    parser.add_argument('--save-checkpoint', '-c', default = True, help='Save a checkpoint file after each validation round')
     parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
                         help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
@@ -164,25 +165,21 @@ def get_mini_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def run_training(image_type):
     args = get_args()
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     logging.info(f'Using device {processor}')
 
-    # Change here to adapt to your data
-    # n_channels=3 for RGB images
-    # n_classes is the number of probabilities you want to get per pixel
-    net = UNet(n_channels=3, n_classes=2, bilinear=True)
+    if image_type == "sar":
+        net = UNet(n_channels=1, n_classes=2, bilinear=True)
+    elif image_type == "modis":
+        net = UNet(n_channels=3, n_classes=2, bilinear=True)
 
     logging.info(f'Network:\n'
                  f'\t{net.n_channels} input channels\n'
                  f'\t{net.n_classes} output channels (classes)\n'
                  f'\t{"Bilinear" if net.bilinear else "Transposed conv"} upscaling')
-
-#    if args.load:
-#        net.load_state_dict(torch.load(args.load, map_location=processor))
-#        logging.info(f'Model loaded from {args.load}')
 
     net.to(device=processor)
     try:
@@ -190,6 +187,7 @@ if __name__ == '__main__':
                   device=processor,
                   epochs=args.epochs, 
                   image_type="modis",
+                  dir_img=tiled512,
                   batch_size=args.batch_size,
                   learning_rate=args.lr,
                   img_scale=args.scale,
@@ -200,3 +198,8 @@ if __name__ == '__main__':
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
         logging.info('Saved interrupt')
         sys.exit(0)
+
+
+# How to run:
+#if __name__ == '__main__':
+#    run_training("modis")
