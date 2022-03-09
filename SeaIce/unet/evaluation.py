fab2cfc34c69cca27ee17043682a6618a80c915f
jonathanroberts <jdr53@cam.ac.uk>
2022-03-09 22:41:41 +0000

    Replaced cross entropy loss function with binary cross entropy.

--- SeaIce/unet/evaluation.py
+++ SeaIce/unet/evaluation.py
@@ -1,8 +1,17 @@
 """ Model evaluation functions and Dice Loss scoring functions """
-from SeaIce.unet.shared import *
+from shared import *
 
 
-def evaluate(net, dataloader, device):
+def evaluate(net, dataloader, device, epsilon):
+    """Evaluate the model during training.
+       Parameters:
+            net: the unet model instance.
+            dataloader: the validation set dataloader object.
+            device: CPU or CUDA.
+            epsilon: weight decay
+       Returns: dice score over the validation set.
+    """
+
     net.eval()
     num_val_batches = len(dataloader)
     dice_score = 0
@@ -14,6 +23,7 @@ def evaluate(net, dataloader, device):
         image = image.to(device=device, dtype=torch.float32)
         mask_true = mask_true.to(device=device, dtype=torch.long)
         mask_true = F.one_hot(mask_true, net.n_classes).permute(0, 3, 1, 2).float()
+        criterion = nn.BCELoss()
 
         with torch.no_grad():
             # predict the mask
@@ -23,22 +33,27 @@ def evaluate(net, dataloader, device):
             if net.n_classes == 1:
                 mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
                 # compute the Dice score
-                dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
+                dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False, epsilon=epsilon) + criterion(mask_pred, mask_true)
             else:
                 mask_pred = F.one_hot(mask_pred.argmax(dim=1), net.n_classes).permute(0, 3, 1, 2).float()
                 # compute the Dice score, ignoring background
-                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true[:, 1:, ...], reduce_batch_first=False)  
-
+                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true[:, 1:, ...], reduce_batch_first=False, epsilon=epsilon) + criterion(mask_pred, mask_true)
     net.train()
+    print(num_val_batches)
+    print(dice_score.item() / num_val_batches)
+
 
     # Fixes a potential division by zero error
     if num_val_batches == 0:
-        return dice_score
-    return dice_score / num_val_batches
+        output = dice_score.item()
+    else:
+        output = dice_score.item() / num_val_batches
+
+    return output
 
 
 def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon=1e-6):
-    # Average of Dice coefficient for all batches, or for a single mask
+    """Average of Dice coefficient for all batches, or for a single mask."""
     assert input.size() == target.size()
     if input.dim() == 2 and reduce_batch_first:
         raise ValueError(f'Dice: asked to reduce batch but got tensor without batch dimension (shape {input.shape})')
@@ -54,12 +69,12 @@ def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, 
         # compute and average metric for each batch element
         dice = 0
         for i in range(input.shape[0]):
-            dice += dice_coeff(input[i, ...], target[i, ...])
+            dice += dice_coeff(input[i, ...], target[i, ...], epsilon=epsilon)
         return dice / input.shape[0]
 
 
 def multiclass_dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon=1e-6):
-    # Average of Dice coefficient for all classes
+    """Average of Dice coefficient for all classes"""
     assert input.size() == target.size()
     dice = 0
     for channel in range(input.shape[1]):
@@ -68,11 +83,11 @@ def multiclass_dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: boo
     return dice / input.shape[1]
 
 
-def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False):
-    # Dice loss (objective to minimize) between 0 and 1
+def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False, epsilon=1e-6):
+    """Dice loss (objective to minimize) between 0 and 1"""
     assert input.size() == target.size()
     fn = multiclass_dice_coeff if multiclass else dice_coeff
-    return 1 - fn(input, target, reduce_batch_first=True)
+    return 1 - fn(input, target, True, epsilon)
 
 
 def view_model(model_path):
@@ -81,4 +96,5 @@ def view_model(model_path):
     model.eval()
     print(model)
     return model
+
 
