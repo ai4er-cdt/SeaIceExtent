500400a3377c921d916fb0725fc7d0b1109f810a
Sophie Turner <st838@cam.ac.uk>
2022-03-09 16:20:55 +0000

    Fn to split train and test tiles.

--- SeaIce/our_code.py
+++ SeaIce/our_code.py
@@ -4,6 +4,7 @@ import controller
 from preprocessing.data_handling import get_contents
 import fiona
 from pathlib import Path
+import os
 
 
 prefix = "G:"
@@ -29,7 +30,6 @@ def make_training_data(all_folder_names, all_folder_paths, all_sizes):
 
              # Look in the first folder
             folder_name, folder_path = all_folder_names[index], all_folder_paths[index]
-
             # Find the shapefile
             shape_file_path = "{}\shapefile\polygon90.shp".format(folder_path)
             try:
@@ -54,16 +54,31 @@ def make_training_data(all_folder_names, all_folder_paths, all_sizes):
             if len(sar_file_names) == 0:
                 sar_file_path = None
             if len(modis_file_names) == 0 and sar_file_path == None:
-               continue
+                continue
             # Old format: 0 = no data. 1 = ice free. 2 = sea ice. 9 = on land or ice shelf. 10 = unclassified.        
             # New format: 0 = ignore (for now). 1 = water. 2 = ice.
             # 1 is already water and 2 is already ice so there is no need to waste time checking or changing these.
-            controller.preprocess_training(shape_file_path, folder_name, modis_file_paths, sar_file_path, size[1], 40, [10, 9], [0, 2], 100, size[0], int(size[0]*0.75))
+            controller.preprocess_training(shape_file_path, folder_name, modis_file_paths, sar_file_path, tiled512, 40, [10, 9], [0, 2], 100, 512, int(512*0.75))
+
+
+def test_split(folder):
+    test_dates = ["2011-01-29", "2011-03-13", "2011-03-30", "2013-02-06"]
+    os.chdir(folder) 
+    for tile in os.listdir():
+        is_test_tile = False
+        for date in test_dates:
+            if date in tile:
+                os.rename(r"{}/{}".format(folder, tile), r"{}/test/{}".format(folder, tile))
+                is_test_tile = True
+                break
+        if "_" in tile and not is_test_tile:
+            os.rename(r"{}/{}".format(folder, tile), r"{}/train/{}".format(folder, tile))
 
 
 all_folder_names, all_folder_paths = get_contents(data, "_", None)
 
-#make_training_data(all_folder_names, all_folder_paths)
+test_split(r"G:\Shared drives\2021-gtc-sea-ice\trainingdata\tiled512")
+#make_training_data(all_folder_names, all_folder_paths, [])
 #controller.start_prediction(r"G:\Shared drives\2021-gtc-sea-ice\data\2011-01-13_021245\MODIS\Antarctica_r05c03.2011013.terra.367.250m.3031.tif")
 
 
