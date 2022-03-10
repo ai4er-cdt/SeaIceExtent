Bug fix for output scaling.

SeaIce/preprocessing/tiling.py
@@ -134,7 +134,7 @@ def tif_to_window(tif_path, window_shape, step_size):
     return image_window
 
 
-def reconstruct_from_tiles(tiles_path, out_path):
+def reconstruct_from_tiles(tiles_path, out_path, scale_up = False):
     """Piece together a mosaic image from tiles ordered by row and col.
        Parameters: 
             tiles_path: (string) directory containing numpy tiles.
@@ -165,6 +165,8 @@ def reconstruct_from_tiles(tiles_path, out_path):
             full_array = row_array
         else:
             full_array = np.concatenate((full_array, row_array), axis=0)
+    if scale_up:
+        full_array *= 100 # Scale up the pixel values so they can be seen. 
     full_image = mask_to_image(full_array)
     full_image.save(out_path)
 