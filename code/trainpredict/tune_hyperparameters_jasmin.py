cd1883ca8c20b90bdba681fae153ba90b5e771d6
jonathanroberts <jdr53@cam.ac.uk>
2022-03-17 09:17:14 +0000

    Cleaned up code and added comments.

--- code/trainpredict/tune_hyperparameters_jasmin.py
+++ code/trainpredict/tune_hyperparameters_jasmin.py
@@ -54,22 +54,22 @@ def build_optimiser(network, config):
 
 
 def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
-    # Inputs for the helper functions
-    img_dir = '/home/users/jdr53/tiled1024/'
-    image_type = 'modis'
 
+    # SET: Conditions for the run.
+    img_dir = '/home/users/jdr53/tiled1024/train/'
+    image_type = 'sar'
     return_type = 'dict'
-    workers = 10
-
-    save_checkpoint = True
+    workers = 4 # MUST BE EVEN
+    save_checkpoint = False
     folder_checkpoint = '/home/users/jdr53/hyp_tuning/checkpoints/'
-
     model_type = 'unet'
     num_output_channels = 1
     loss_function = "BCE"
 
+    # Setting the device to GPU if available or CPU if not.
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Creating the network
     if image_type == "sar":
         is_single_band = True
         net = UNet(1, num_output_channels, True)
@@ -79,35 +79,36 @@ def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
     else:
         raise Exception(f'Invalid image type detected: {image_type}. Expected "sar" or "modis".')
 
+    # Loading the network to the device (GPU or CPU)
     net.to(device)
 
-    # Initialize a new wandb run
+    # Initialising a new wandb run
     with wandb.init(config=config):
-        # If called by wandb.agent, as below,
-        # this config will be set by Sweep Controller
+        # If called by wandb.agent, as below, this config will be set by Sweep Controller
         config = wandb.config
 
-        # Loader
+        # Getting list of image-mask pairs
         img_list = create_npy_list(img_dir, image_type)
 
+        # Splitting the list into a training and validation set.
         train_img_list, val_img_list, n_train, n_val = split_img_list(img_list, config.validation_percent)
 
+        # Creating training and validation datasets; the training dataset is augmented, the validation set it not.
         train_dataset = CustomImageAugmentDataset(train_img_list, is_single_band, return_type, True)
         validation_dataset = CustomImageAugmentDataset(val_img_list, is_single_band, return_type, False)
 
+        # Creating dataloaders for each dataset.
         train_loader, val_loader = create_dataloaders(train_dataset, validation_dataset, config.batch_size, workers)
 
-        # Optimiser
+        # Configuring the optimiser and learning rate scheduler
         optimiser = build_optimiser(net, config)
-
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'max',
-                                                         patience=2)  # CHECK goal: maximize Dice score
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'max', patience=2)
         grad_scaler = torch.cuda.amp.GradScaler(enabled=False)
         global_step = 0
-        # epoch_step
 
         # Begin training
         run_loss_train = 0
+        # Selecting form of the cross-entropy loss term
         if loss_function == "BCE":
             criterion = nn.BCELoss()
         elif loss_function == "CE":
@@ -115,12 +116,15 @@ def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
         else:
             raise Exception(f'Invalid loss_function detected: {loss_function}. Expected "BCE" or "CE".')
 
+        # Iterating training and validation over epochs.
         for epoch in range(config.epochs):
-            net.train()
+
+            net.train() # Setting the network to train
             epoch_loss = 0
             n_batches = 0
             with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{config.epochs}', unit='img') as pbar:
 
+                # Iterating over training batches
                 for batch in train_loader:
                     images = batch['image']
                     true_masks = batch['mask']
@@ -134,31 +138,34 @@ def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
                     true_masks = true_masks.to(device=device, dtype=torch.long)
 
                     with torch.cuda.amp.autocast(enabled=amp):
-
+                        # Making predictions
                         mask_pred = net(images)
+                        # If 1 channel output is required
                         if net.n_classes == 1:
-                            # mask_pred = torch.flatten(mask_pred, start_dim=1, end_dim=2)
+                            # Applying a sigmoid function to the true and predicted masks.
                             true_masks = (torch.sigmoid(true_masks) > 0.5).float()
                             mask_pred = (torch.sigmoid(mask_pred) > 0.5).float()
-                            if loss_function == "BCE":
-                                mask_pred.requires_grad = True
+                            mask_pred.requires_grad = True
+                            # Adding a dummy dimension to match with the shape of the predicted masks
                             true_masks = true_masks[:, None, :, :]
-                            # compute the Dice score
+                            # Computing the loss
                             loss = dice_loss(mask_pred, true_masks, multiclass=False,
                                              epsilon=config.weight_decay) + criterion(mask_pred, true_masks)
+                        # If 2 channel output is required
                         else:
+                            # One-hot encoding the masks
                             true_masks = F.one_hot(true_masks, net.n_classes).permute(0, 3, 1, 2).float()
                             if loss_function == "BCE":
-                                mask_pred = F.one_hot(mask_pred.argmax(dim=1), net.n_classes).permute(0, 3, 1,
-                                                                                                      2).float()
+                                mask_pred = F.one_hot(mask_pred.argmax(dim=1),
+                                                      net.n_classes).permute(0, 3, 1, 2).float()
                                 mask_pred.requires_grad = True
                             else:
                                 mask_pred = F.softmax(mask_pred, dim=1).float()
-                            # compute the Dice score, ignoring background
+                            # # Computing the loss
                             loss = dice_loss(mask_pred, true_masks, multiclass=True,
                                              epsilon=config.weight_decay) + criterion(mask_pred, true_masks)
 
-                        # Optimisation
+                    # Optimisation & Training
                     optimiser.zero_grad(set_to_none=True)
                     grad_scaler.scale(loss).backward()
                     grad_scaler.step(optimiser)
@@ -168,19 +175,19 @@ def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
                     global_step += 1
                     batch_loss = loss.item()
                     epoch_loss += batch_loss
+                    # Logging batch loss
                     wandb.log({"Batch Loss, Training": batch_loss}, step=global_step)
                     pbar.set_postfix(**{'loss (batch)': loss.item()})
                     n_batches += 1
+
+                # Validation
                 val_score = evaluate(net, val_loader, device, config.weight_decay, loss_function)
 
                 print(f'\nVal Score: {val_score}, Epoch: {epoch}')
-
-                # wandb.log({"Batch Loss, Validation": val_score_list[index]}, step=global_step-(len(val_score_list)+index))
-                # wandb.log({"Epoch Validation Loss": val_score}) , step=global_step-(len(val_score_list)+index)
                 scheduler.step(val_score)
 
+            # Calculating epoch training loss
             avg_epoch_training_loss = epoch_loss / n_batches
-            # wandb.log({"Epoch Training Loss": avg_epoch_training_loss})
             run_loss_train += avg_epoch_training_loss
             # run_loss_val += val_score
             print('Logging Epoch Scores')
@@ -194,7 +201,7 @@ def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
                 if val_score < best_run_loss_val:
                     best_run_loss_val = val_score
                     best_epoch = epoch
-
+            # Saving model checkpoint
             if save_checkpoint:
                 if epoch == 0:
                     dir_checkpoint = create_checkpoint_dir(folder_checkpoint, image_type, model_type, config.optimiser,
@@ -204,26 +211,10 @@ def train_and_validate(config=None, amp=False, device=torch.device('cuda')):
         wandb.log({"Run Loss, Training": run_loss_train / config.epochs, "Run Loss, Validation": best_run_loss_val,
                    "Run Best Epoch": best_epoch}, step=global_step)
 
-        # wandb.log({"Run Training Loss": run_loss_train})
-        # wandb.log({"Run Validation Loss": run_loss_val})
-        # wandb.log({"loss": run_loss_train})
-
-
-# Improvements:
-# Remove Epoch log -- DONE
-# Validation metrics are 0.
-# Loss in chart is 0 -- DONE
-
-
 
 if __name__ == '__main__':
 
-    #wandb.init(project="test-hyptuning")
-
-    # Model Name
-    #model_name = 'unet'
-
-    # Configuring wandb settings
+    # Configuring wandb sweep settings
     sweep_config = {
         'method': 'random',  # Random search method -- less computationally expensive yet effective.
     }
@@ -249,8 +240,8 @@ if __name__ == '__main__':
         'batch_size': {
             # Uniformly-distributed between 5-15
             'distribution': 'int_uniform',
-            'min': 5,
-            'max': 25
+            'min': 1,
+            'max': 5
         }
     }
     sweep_config['parameters'] = parameters_dict
@@ -269,10 +260,14 @@ if __name__ == '__main__':
             'value': 1e-8}
     })
 
-    sweep_id = wandb.sweep(sweep_config, project="jasmin_gpu_13_02")
+    sweep_id = wandb.sweep(sweep_config, project="jasmin_gpu_15_03_tuning")
 
+    # Number of runs 
     n_tuning = 1000
+
+    # Initialising the wandb agent
     wandb.agent(sweep_id, function=train_and_validate, count=n_tuning)
+
 
 
 
